20a75beaebfdd81adc593adb6fb860a682b466e3
Al3xTO <147641630+Al3xTO@users.noreply.github.com>
2024-04-17 21:01:55 +0200

    Add files via upload

---
+++ Post/Post/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Post
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelpoint = new System.Windows.Forms.Label();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.labelhouse = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Post.Properties.Resources.house;
+            this.pictureBox1.Location = new System.Drawing.Point(641, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Post.Properties.Resources.house;
+            this.pictureBox2.Location = new System.Drawing.Point(419, 76);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Post.Properties.Resources.house;
+            this.pictureBox3.Location = new System.Drawing.Point(204, 76);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Post.Properties.Resources.house;
+            this.pictureBox4.Location = new System.Drawing.Point(204, 264);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Post.Properties.Resources.house;
+            this.pictureBox5.Location = new System.Drawing.Point(419, 264);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Post.Properties.Resources.house;
+            this.pictureBox6.Location = new System.Drawing.Point(641, 264);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(669, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Дім 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(447, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Дім 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Дім 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(669, 367);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Дім 6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(447, 367);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Дім 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(232, 367);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Дім 4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(641, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Опит:";
+            // 
+            // labelpoint
+            // 
+            this.labelpoint.AutoSize = true;
+            this.labelpoint.Location = new System.Drawing.Point(682, 13);
+            this.labelpoint.Name = "labelpoint";
+            this.labelpoint.Size = new System.Drawing.Size(35, 13);
+            this.labelpoint.TabIndex = 13;
+            this.labelpoint.Text = "label8";
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::Post.Properties.Resources.box;
+            this.pictureBox7.Location = new System.Drawing.Point(35, 202);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 15;
+            this.pictureBox7.TabStop = false;
+            // 
+            // labelhouse
+            // 
+            this.labelhouse.AutoSize = true;
+            this.labelhouse.Location = new System.Drawing.Point(13, 13);
+            this.labelhouse.Name = "labelhouse";
+            this.labelhouse.Size = new System.Drawing.Size(35, 13);
+            this.labelhouse.TabIndex = 16;
+            this.labelhouse.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(13, 41);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(107, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Управління на ЛКМ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.labelhouse);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.labelpoint);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelpoint;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label labelhouse;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Post/Post/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Post
+{
+    public partial class Form1 : Form, IObserver
+    {
+        private bool isDragging = false;
+        private Point lastLocation;
+        private int randomNumber;
+        private Timer timer1;
+        private Timer timer2;
+        private Subject subject = new Subject();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            subject.Subscribe(this);
+
+            pictureBox7.MouseDown += PictureBox7_MouseDown;
+            pictureBox7.MouseMove += PictureBox7_MouseMove;
+            pictureBox7.MouseUp += PictureBox7_MouseUp;
+
+            pictureBox1.MouseEnter += PictureBox_MouseEnter;
+            pictureBox2.MouseEnter += PictureBox_MouseEnter;
+            pictureBox3.MouseEnter += PictureBox_MouseEnter;
+            pictureBox4.MouseEnter += PictureBox_MouseEnter;
+            pictureBox5.MouseEnter += PictureBox_MouseEnter;
+            pictureBox6.MouseEnter += PictureBox_MouseEnter;
+
+            this.MouseUp += Form1_MouseUp;
+
+            timer1 = new Timer();
+            timer1.Interval = 100;
+            timer1.Tick += Timer1_Tick;
+
+            timer2 = new Timer();
+            timer2.Interval = 2000;
+            timer2.Tick += Timer2_Tick;
+
+            GenerateRandomNumber();
+            labelpoint.Text = "0xp";
+        }
+
+        public void Update(int randomNumber)
+        {
+            labelhouse.Text = $"Доставити у дім: {randomNumber}";
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox7.Location = new Point(2000, 2000);
+            timer1.Stop();
+            timer2.Start();
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox7.Location = new Point(13, 200);
+            timer2.Stop();
+            GenerateRandomNumber();
+        }
+
+        private void PictureBox7_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            lastLocation = e.Location;
+        }
+
+        private void PictureBox7_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                pictureBox7.Left += e.X - lastLocation.X;
+                pictureBox7.Top += e.Y - lastLocation.Y;
+            }
+        }
+
+        private void PictureBox7_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+
+            foreach (Control control in Controls)
+            {
+                if (control is PictureBox pictureBox && pictureBox != pictureBox7 && pictureBox.Bounds.Contains(pictureBox7.Location))
+                {
+                    if (randomNumber == int.Parse(pictureBox.Name.Substring(10)))
+                    {
+                        labelpoint.Text = (int.Parse(labelpoint.Text.Substring(0, labelpoint.Text.Length - 2)) + 10).ToString() + "xp";
+                        label10.Text = "Посилку доставлено успішно";
+                    }
+                    else
+                    {
+                        labelpoint.Text = (int.Parse(labelpoint.Text.Substring(0, labelpoint.Text.Length - 2)) - 5).ToString() + "xp";
+                        label10.Text = "Посилка прийшла не на ту адресу";
+                    }
+                    break;
+                }
+            }
+
+            timer1.Start();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void PictureBox_MouseEnter(object sender, EventArgs e)
+        {
+            PictureBox pictureBox = sender as PictureBox;
+            pictureBox.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void GenerateRandomNumber()
+        {
+            Random random = new Random();
+            randomNumber = random.Next(1, 7);
+            labelhouse.Text = $"Доставити у дім: {randomNumber}";
+
+            subject.Notify(randomNumber);
+        }
+    }
+
+    public interface IObserver
+    {
+        void Update(int randomNumber);
+    }
+
+    public class Subject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+
+        public void Subscribe(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Unsubscribe(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify(int randomNumber)
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(randomNumber);
+            }
+        }
+    }
+}
+
